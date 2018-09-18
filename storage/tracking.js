f66b7b356722bba1271060712b509b59315e1458
jinh0park <jinh0park@naver.com>
2018-09-18 14:34:42 +0900

    storage

--- storage/tracking.js
+++ storage/tracking.js
@@ -17,3 +17,9 @@ var getCookie = function(name) {
   var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
   return value? value[2] : null;
 };
+
+var setCookie = function(name, value, exp) {
+  var date = new Date();
+  date.setTime(date.getTime() + exp*24*60*60*1000);
+  document.cookie = namae + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+};
