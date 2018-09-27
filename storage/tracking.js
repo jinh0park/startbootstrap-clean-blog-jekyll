188f9a5fdcc74617ffe26078f647a57d65708c75
jinh0park <jinh0park@naver.com>
2018-09-27 13:39:04 +0900

    removed unnecessary? files

--- storage/tracking.js
+++ storage/tracking.js
@@ -50,7 +50,7 @@ var orderResult = function() {
 
 var getParameterByName = function(name, url) {
   if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, '\\$&');
+  //name = name.replace(/[\[\]]/g, '\\$&');
   var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
     results = regex.exec(url);
   if (!results) return null;
