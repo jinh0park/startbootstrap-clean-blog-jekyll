817adbafab45ab764d4275a7ef5244c9abe9fe58
jinh0park <jinh0park@naver.com>
2018-09-27 13:42:51 +0900

    removed unnecessary? files

--- storage/tracking.js
+++ storage/tracking.js
@@ -56,7 +56,7 @@ var getParameterByName = function(name, url) {
   if (!results) return null;
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
+};
 
 //모든 페이지에서 Default로 실행되는 함수
 (function() {
