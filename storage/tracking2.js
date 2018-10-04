b7a1a9c24abaad33f43861b137e969633da7505f
DESKTOP-D3D299N\IGAWorks <jinh0park@naver.com>
2018-10-04 10:55:13 +0900

    no message

--- storage/tracking2.js
+++ storage/tracking2.js
@@ -1,1 +1,6 @@
-console.log(window.location.href)
+window.addEventListener('message', function(e) {
+  console.log(e.data); // { hello: 'parent' }
+  var item = window.location.href;
+  console.log(item); // zerocho
+  document.getElementById('iframe').contentWindow.postMessage(item, '*');
+});
