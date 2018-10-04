f403ecd8305d6ae8533cb95d5d2635523690988f
DESKTOP-D3D299N\IGAWorks <jinh0park@naver.com>
2018-10-04 16:23:10 +0900

    no message

--- storage/tracking2.js
+++ storage/tracking2.js
@@ -1,4 +1,56 @@
-window.addEventListener('message', function(e) {
-  var item = escape(window.location.href);
-  document.getElementById('iframe').contentWindow.postMessage("url="+item, '*');
-});
+(function() {
+  window.addEventListener('message', function(e) {
+    var url = escape(window.location.href);
+
+    var getCookie = function(name) {
+      var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+      return value ? value[2] : null;
+    };
+
+    var setCookie = function(name, value, exp) {
+      var date = new Date();
+      date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+      document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+    };
+
+    var issueID = function() {
+
+      var date = new Date()
+      exp = 7;
+      date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+      var value = `ZERO.${date.getTime()}.${Math.round(Math.random()*1000000000)}`;
+      document.cookie = '_zeroid=' + value + ';expires=' + date.toUTCString() + ';path=/';
+    };
+
+
+    var getParameterByName = function(name, url) {
+      if (!url) url = window.location.href;
+
+      var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return '';
+      return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    };
+
+
+    var msg = (function() {
+      data = {};
+      _zeroid = getCookie('_zeroid');
+
+      if (!_zeroid) {
+        last_camp = getParameterByName('iap');
+        if (!last_camp) return;
+        data.last_camp = last_camp;
+        issueID();
+        _zeroid = getCookie('_zeroid');
+      }
+      last_camp = getParameterByName('iap');
+      if (last_camp) data.last_camp = last_camp;
+      data._zeroid = _zeroid;
+      data.url = escape(window.location.href);
+      return `url=${data.url}&_zeroid=${data._zeroid}&last_camp${data.last_camp}`
+    })();
+    document.getElementById('iframe').contentWindow.postMessage(msg, '*');
+  });
+})();
