f37fff4b1c5228be5ee736583d2e6a1dc1206172
DESKTOP-D3D299N\IGAWorks <jinh0park@naver.com>
2018-10-04 16:05:11 +0900

    json escape

--- storage/tracking2.js
+++ storage/tracking2.js
@@ -1,6 +1,4 @@
 window.addEventListener('message', function(e) {
-  console.log(e.data); // { hello: 'parent' }
-  var item = window.location.href;
-  console.log(item); // zerocho
+  var item = escape(window.location.href);
   document.getElementById('iframe').contentWindow.postMessage(item, '*');
 });
