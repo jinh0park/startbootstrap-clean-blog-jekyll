81272f2938c2d4aa1d7140caa862d2e7533503ca
jinh0park <jinh0park@naver.com>
2019-05-21 11:54:08 +0900

    fix

--- ssd/index.js
+++ ssd/index.js
@@ -8,14 +8,44 @@ var model;
 })
 
 var execute = function(){
+  var box;
   (() => {
     console.time("q");
-    model.executeAsync(tf.zeros([1, 1000, 1000, 3])).then(
+    var context = document.getElementById('blah');
+    var img = tf.browser.fromPixels(context);
+    [h,w,c] = img.shape;
+    img = img.reshape([1,h,w,c]);
+    model.executeAsync(img).then(
       function(data) {
         console.log(
           data.slice([0, 0], [1, 1]).squeeze().arraySync()
         );
+        box = data.slice([0, 0], [1, 1]).squeeze().arraySync()
+        boxing(h, w, box);
         console.timeEnd("q")
       })
   })();
+}
+
+var b;
+
+var boxing = function (h, w, box) {
+  b = box;
+  console.log(box);
+  var c = document.getElementById("myCanvas");
+  c.height = h;
+  c.width = w;
+  var ctx = c.getContext("2d");
+  var img = document.getElementById("blah");
+  ctx.drawImage(img, 0, 0);
+
+  [h1_, w1_, h2_, w2_] = box;
+  h1 = parseInt(h * h1_);
+  w1 = parseInt(w * w1_);
+  h2 = parseInt(h * h2_);
+  w2 = parseInt(w * w2_);
+
+  ctx.rect(w1, h1, w2-w1, h2-h1);
+  ctx.stroke();
+
 }
